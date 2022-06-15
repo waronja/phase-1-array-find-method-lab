935aacb55a921541e1195e88a4b7d5f40163e664
simon waronja <simon.waronja@student.moringaschool.com>
2022-06-15 23:48:41 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+let superbowlWin = (record) => {
+
+    let result = record.find(record => record.result === "W");
+
+    if (result) {
+        return result.year;
+    } else {
+        return undefined;
+    }
+}
